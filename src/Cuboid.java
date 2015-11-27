23f5d6cfe9b4bbcec2c3a09e818bb4e2f6e37a50
Thomas Cocher <thomas.cocher@student.ecp.fr>
2015-11-27 22:30:06 +0100

    Ajout de classes Cuboid, ConsultationType, Date,Test et modif de Item

---
+++ src/Cuboid.java
@@ -0,0 +1,32 @@
+
+public class Cuboid {
+	private double lenght;
+	private double height;
+	private double width;
+	public Cuboid(double lenght, double height, double width) {
+		super();
+		this.lenght = lenght;
+		this.height = height;
+		this.width = width;
+	}
+	public double getLenght() {
+		return lenght;
+	}
+	public void setLenght(double lenght) {
+		this.lenght = lenght;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	
+}
