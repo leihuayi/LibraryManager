62d8c25e65fb48b74fec981753c664816ba9bb5d
Thomas Cocher <thomas.cocher@student.ecp.fr>
2015-11-27 22:51:07 +0100

    Changement de nom Item et ajout Book, CD, DVD

---
+++ src/Book.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+
+public class Book extends LibraryItem {
+	
+	private int ISBNCode;
+	private int borrowingDuration;
+
+	public int getISBNCode() {
+		return ISBNCode;
+	}
+
+	public void setISBNCode(int iSBNCode) {
+		ISBNCode = iSBNCode;
+	}
+
+	public int getBorrowingDuration() {
+		return borrowingDuration;
+	}
+
+	public void setBorrowingDuration(int borrowingDuration) {
+		this.borrowingDuration = borrowingDuration;
+	}
+
+	public Book(String title, String publisher, int publishingYear, int volumeNumber, ConsultationType consultationType,
+			Date borrowingDeadline, Cuboid measures, Array location, int iSBNCode) {
+		super(title, publisher, publishingYear, volumeNumber, consultationType, borrowingDeadline, measures, location);
+		ISBNCode = iSBNCode;
+		this.borrowingDuration=4;
+	}
+
+		
+	
+	
+}
---
+++ src/DVD.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+
+public class DVD extends LibraryItem {
+	private int borrowingDuration;
+
+	public int getBorrowingDuration() {
+		return borrowingDuration;
+	}
+
+	public void setBorrowingDuration(int borrowingDuration) {
+		this.borrowingDuration = borrowingDuration;
+	}
+
+	public DVD(String title, String publisher, int publishingYear, int volumeNumber, ConsultationType consultationType,
+			Date borrowingDeadline, Cuboid measures, Array location) {
+		super(title, publisher, publishingYear, volumeNumber, consultationType, borrowingDeadline, measures, location);
+		this.borrowingDuration=2;
+	}
+	
+}
