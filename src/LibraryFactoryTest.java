3302ead8dec71c46a551ee5863f21e11d72a93de
Sarah Gross <sarah.gross@student.ecp.fr>
2015-12-22 16:50:53 +0100

    enrichissement du test AnyFit

--- src/LibraryFactoryTest.java
+++ src/LibraryFactoryTest.java
@@ -49,6 +49,7 @@ public class LibraryFactoryTest {
 		Bookcase bookcasebis = new Bookcase("bookcase", 5,5,5);
 		room.getListBookcases().add(bookcase);
 		room.getListBookcases().add(bookcasebis);
+		//shelf, shelfbis and shelfter have more space than shelf2
 		Shelf shelf = new Shelf(3,3,3);
 		Shelf shelfbis=new Shelf(2,2,2);
 		Shelf shelfter=new Shelf(2,2,2);
@@ -59,13 +60,19 @@ public class LibraryFactoryTest {
 		bookcasebis.getListShelves().add(shelfquar);
 		Date date=new Date();
 		Room roombis=new Room("room",5,5,5);
+		/*
+		 * but the room of shelf2 is placed before the room of the other shelves => shelf2
+		 *  is the first shelf to be found with enough space
+		 */
 		library.getListRooms().add(0,roombis);;
 		Bookcase bookcase2=new Bookcase("bookcase",4,4,4);
 		roombis.getListBookcases().add(bookcase2);
-		Shelf shelf2=new Shelf(2,2,2);
-		bookcase2.getListShelves().add(shelf2);
+		Shelf shelf1 = new Shelf(0.5,1,1);
+		Shelf shelf2=new Shelf(1,1,1);
+		//the first shelf in this room is shelf1 but is is too small to host the CD
+		bookcase2.getListShelves().add(shelf1);
+		bookcase2.getListShelves().add(1,shelf2);
 		CD CD =new CD("CD","Alexandre Prot",2015,1,ConsultationType.borrowing,date,1,1,1,null);
-		BestRoom strategy = new BestRoom();
 		Test.store_items(library, "anyfit");
 		assertTrue(shelf2.getListItems().contains(CD));
 		//assertTrue(shelf.getListItems().contains(CD)||shelfter.getListItems().contains(CD)||shelfquar.getListItems().contains(CD)||shelfbis.getListItems().contains(CD));
