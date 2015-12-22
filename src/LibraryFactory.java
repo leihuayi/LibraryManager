125a78c48b830fd4cfede66d43dd97e05b243f42
Sarah Gross <sarah.gross@student.ecp.fr>
2015-12-22 15:00:30 +0100

    ajout des méthodes à implémenter à la libraryFactory

--- src/LibraryFactory.java
+++ src/LibraryFactory.java
@@ -4,6 +4,28 @@ public class LibraryFactory {
 	public Library create_library(String name, int nbi, int n, int m, int mp){
 			return (new Library(name,nbi,n,m,mp));
 	}
+	
+	public void add_room(Library lib, String roomName, double length, double height, double width){
+	}
+	
+	public void add_bookcase(){
+	}
+	
+	public void add_item(){
+	}
+	
+	public void store_items(){
+	}
+	
+	public void unstore_items(){
+	}
+	
+	public String list_items(){
+		return "";
+	}
+	
+	
+	
 
 	
 }
