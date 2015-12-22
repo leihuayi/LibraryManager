ec935b8f5ef800dff8350a08d6687bc098479a58
Sarah Gross <sarah.gross@student.ecp.fr>
2015-12-22 15:08:37 +0100

    préparation de tous les tests et méthodes à implémenter

--- src/LibraryFactory.java
+++ src/LibraryFactory.java
@@ -24,8 +24,30 @@ public class LibraryFactory {
 		return "";
 	}
 	
+	public String list_room(){
+		return "";
+	}
 	
+	public String list_bookcase(){
+		return "";
+	}
 	
-
+	public String find_items(){
+		return "";
+	}
+	
+	public String search_title(){
+		return "";
+	}
+	
+	public void add_member(){
+	}
+	
+	public void borrow_item(){
+	}
+	
+	public String check_borrowed(){
+		return "";
+	}
 	
 }
