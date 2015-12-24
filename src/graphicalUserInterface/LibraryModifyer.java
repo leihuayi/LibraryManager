7956c4a4b3e93501972b0a1ecce6a05be2055200
Sarah Gross <sarah.gross@student.ecp.fr>
2015-12-24 19:11:38 +0100

    add panel

--- src/graphicalUserInterface/LibraryModifyer.java
+++ src/graphicalUserInterface/LibraryModifyer.java
@@ -54,8 +54,6 @@ public class LibraryModifyer extends JFrame {
 	    		catch(IOException i){
 	    			JOptionPane.showMessageDialog(LibraryModifyer.this,"Whoops there must have been a bug in the retrieval. Please try again.");
 	    		}
-
-	    		JOptionPane.showMessageDialog(LibraryModifyer.this,"saved!");
 	             
 	            }
 	        });
@@ -73,9 +71,52 @@ public class LibraryModifyer extends JFrame {
 		//Tab : Add
 		
 		JPanel panelAdd = new JPanel();
+		panelAdd.setLayout(new BoxLayout(panelAdd,BoxLayout.PAGE_AXIS));
 		tabbedPane.addTab("Add panel", panelAdd);
-		panelAdd.add(new JLabel("Add information"));
 		this.setContentPane(tabbedPane);
+		panelAdd.add(new JLabel("Chose which kind of element you want to add to your library"));
+		final String addRoom = "add room <library name> : to add a room to a library with given name";
+		final String addBc = "add bookcase <num shelves, room name> : to add a bookcase with num shelves to a given room of the library";
+		final String addItem = "add item <list of parameters>: to add a library item with given parameters to the temporary storage box of library";
+		final String addMember = "add member <member name, ccard num, email, membership type> : to add a new member to the library";
+		final String[] addChoice = {addRoom,addBc,addItem,addMember};
+		JComboBox<String> comboBox = new JComboBox<String>(addChoice);
+		panelAdd.add(comboBox);
+		JButton buttonNextAdd = new JButton("Next");
+	    buttonNextAdd.addActionListener(new ActionListener(){
+	    	
+
+	  	  
+	    	@Override
+	        public void actionPerformed(ActionEvent ae) {
+	    		String selectedAdd = (String) comboBox.getSelectedItem();
+	    		
+	    		switch(selectedAdd){
+	    		case addRoom :
+	    			JFrame frame = new JFrame();
+	    			String input = JOptionPane.showInputDialog(frame,"what ?","Parameters of add room",
+	    					JOptionPane.QUESTION_MESSAGE);
+	    			break;
+	    			
+	    		case addBc :
+	    			
+	    			break;
+	    		
+	    		case addItem :
+	    			
+	    			break;	
+	    			
+	    		case addMember :
+	    			
+	    			break;
+	    			
+	    		}
+	    
+	            }
+	        });
+	    
+	    panelAdd.add(buttonNextAdd);
+
 		
 		
 		
